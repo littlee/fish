c72fac235329feeecfabff2e768fc6b811bf8baa
littlee <kli@gizwits.com>
2017-06-14 00:48:55 +0800

    sifh

--- js/playState.js
+++ js/playState.js
@@ -6,6 +6,21 @@ var textStyle = {
   fill: '#fff'
 }
 
+var fishWidth = {
+  f1: 200,
+  f2: 200,
+  f3: 143,
+  f4: 143,
+  f5: 109,
+  f6: 109,
+  f7: 64,
+  f8: 64,
+  f9: 47,
+  f10: 47,
+  f11: 55,
+  f12: 55
+}
+
 function randomInt(a, b) {
   return game.rnd.integerInRange(a, b)
 }
@@ -14,30 +29,66 @@ function isHeadOnLeft(key) {
   return parseInt(key.slice(1), 10) % 2 !== 0
 }
 
+function getInitFishX(headOnLeft, key) {
+  return headOnLeft
+    ? 640 + fishWidth[key] / 2 - 10
+    : 0 - fishWidth[key] / 2 + 10
+}
+
+function getInitFishY() {
+  return randomInt(500, 800)
+}
+
+function getInitFishVelocity(headOnLeft) {
+  return headOnLeft ? -1 * randomInt(10, 100) : randomInt(10, 100)
+}
+
+function getInitFishGravity(headOnLeft) {
+  return headOnLeft ? -10 : 10
+}
+
 var playState = {
   create: function() {
     this.bg = game.add.image(0, 0, 'bg')
+
+    this.fishPole = game.add.sprite(game.world.width, 300, 'fish_pole')
+    this.fishPole.anchor.set(1, 0.5)
+    this.fishPole.angle = 90
+
+    this.jackpot = game.add.image(420, 100, 'jackpot')
+
+    this.jackpotText = game.add.text(0, 0, this._jackpot, {
+      fontSize: '32px',
+      fill: '#F8F042',
+      boundsAlignH: 'center',
+      boundsAlignV: 'middle'
+    })
+    this.jackpotText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5)
+    this.jackpotText.setTextBounds(440, 168, 170, 32)
 
     this.fishes = game.add.group()
     this.fishes.enableBody = true
     this.fishes.physicsBodyType = Phaser.Physics.ARCADE
     for (var i = 0; i < 10; i++) {
       var r = randomInt(1, 12)
-      var headOnLeft = r % 2 !== 0 // 单数
-      var x = headOnLeft ? randomInt(580, 620) : randomInt(-20, 20)
-      var y = randomInt(500, 800)
-      var fishItem = this.fishes.create(x, y, 'f' + r)
-      fishItem.body.velocity.x = headOnLeft ? -1 * randomInt(0, 100) : randomInt(0, 100)
-      fishItem.body.gravity.x = 0
-      fishItem.checkWorldBounds = true
+      var headOnLeft = r % 2 !== 0
+      var key = 'f' + r
+      var x = getInitFishX(headOnLeft, key)
+      var y = getInitFishY()
+      var fishItem = this.fishes.create(x, y, key)
+      fishItem.body.velocity.x = getInitFishVelocity(headOnLeft)
+      fishItem.body.gravity.x = getInitFishGravity(headOnLeft)
     }
     this.fishes.callAll('animations.add', 'animations', 'swim')
     this.fishes.callAll('animations.play', 'animations', 'swim', 10, true)
     this.fishes.setAll('checkWorldBounds', true)
     this.fishes.setAll('outOfBoundsKill', true)
+    this.fishes.setAll('anchor.x', 0.5)
+    this.fishes.setAll('anchor.y', 0.5)
 
-    game.time.events.loop(500, this._updateFishes, this)
-
+    game.time.events.loop(1000, this._updateFishesMove, this)
+    game.time.events.loop(500, this._updateFishesAdd, this)
+    game.time.events.loop(500, this._updateFishesCatch, this)
 
     this.betPop = game.add.group()
     this.betPopBg = game.add.sprite(53, 650, 'bet_pop')
@@ -69,12 +120,30 @@ var playState = {
     this.betPop50Text.inputEnabled = true
     this.betPop10Text.inputEnabled = true
 
-    this.betPop1000Text.events.onInputDown.add(this._changeBet.bind(this, 1000), this)
-    this.betPop500Text.events.onInputDown.add(this._changeBet.bind(this, 500), this)
-    this.betPop200Text.events.onInputDown.add(this._changeBet.bind(this, 200), this)
-    this.betPop100Text.events.onInputDown.add(this._changeBet.bind(this, 100), this)
-    this.betPop50Text.events.onInputDown.add(this._changeBet.bind(this, 50), this)
-    this.betPop10Text.events.onInputDown.add(this._changeBet.bind(this, 10), this)
+    this.betPop1000Text.events.onInputDown.add(
+      this._changeBet.bind(this, 1000),
+      this
+    )
+    this.betPop500Text.events.onInputDown.add(
+      this._changeBet.bind(this, 500),
+      this
+    )
+    this.betPop200Text.events.onInputDown.add(
+      this._changeBet.bind(this, 200),
+      this
+    )
+    this.betPop100Text.events.onInputDown.add(
+      this._changeBet.bind(this, 100),
+      this
+    )
+    this.betPop50Text.events.onInputDown.add(
+      this._changeBet.bind(this, 50),
+      this
+    )
+    this.betPop10Text.events.onInputDown.add(
+      this._changeBet.bind(this, 10),
+      this
+    )
 
     this.betPop.add(this.betPopBg)
     this.betPop.add(this.betPop1000Coin)
@@ -94,7 +163,11 @@ var playState = {
     this.woodTable = game.add.image(0, game.world.height, 'wood_table')
     this.woodTable.anchor.set(0, 1)
 
-    this.throwBtn = game.add.sprite(game.world.centerX, game.world.height - 15, 'throw_btn')
+    this.throwBtn = game.add.sprite(
+      game.world.centerX,
+      game.world.height - 15,
+      'throw_btn'
+    )
     this.throwBtn.anchor.set(0.5, 1)
     this.throwBtn.animations.add('push')
     this.throwBtn.inputEnabled = true
@@ -107,23 +180,12 @@ var playState = {
     this.notchBetText = game.add.text(92, 959, this._currentBet, textStyle)
 
     this.notchWin = game.add.image(430, 942, 'notch_win')
-    this.notchWinText = game.add.text(483, 959, '赢得: ' + this._currentWin, textStyle)
-
-    this.fishPole = game.add.sprite(game.world.width, 300, 'fish_pole')
-    this.fishPole.anchor.set(1, 0.5)
-    this.fishPole.angle = 90
-
-    this.jackpot = game.add.image(420, 100, 'jackpot')
-
-    this.jackpotText = game.add.text(0, 0, this._jackpot, {
-      fontSize: '32px',
-      fill: '#F8F042',
-      boundsAlignH: 'center',
-      boundsAlignV: 'middle'
-    })
-    this.jackpotText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 5)
-    this.jackpotText.setTextBounds(440, 168, 170, 32)
-
+    this.notchWinText = game.add.text(
+      483,
+      959,
+      '赢得: ' + this._currentWin,
+      textStyle
+    )
 
     this.currentCoin = game.add.image(110, 20, 'current_coin')
     this.currentCoinText = game.add.text(0, 0, this._currentCoin, {
@@ -132,7 +194,7 @@ var playState = {
       boundsAlignH: 'center',
       boundsAlignV: 'middle'
     })
-    this.currentCoinText.setTextBounds(172, 35 ,110, 44)
+    this.currentCoinText.setTextBounds(172, 35, 110, 44)
 
     this.luckyValue = game.add.image(30, 70, 'lucky_value')
     this.luckyValuePro = game.add.image(103, 100, 'lucky_value_p')
@@ -146,6 +208,7 @@ var playState = {
     })
     this.userName.setTextBounds(10, 70, 85, 24)
 
+    this.fishCountGroup = game.add.group()
     this.fishCount = game.add.sprite(0, 285, 'fish_count')
     this.fishCountText = game.add.text(95, 265, this._fishCount, {
       fill: '#fff',
@@ -154,6 +217,10 @@ var playState = {
       fontSize: 64
     })
     this.fishCountText.setShadow(2, 2, 'rgba(6,68,142,0.7)', 1)
+    this.fishCountGroup.add(this.fishCount)
+    this.fishCountGroup.add(this.fishCountText)
+    this.fishCountGroup.x = -187
+    this.fishCountGroup.alpha = 0
 
     this.fishScoreText = game.add.text(15, 180, '+ ' + this._fishScore, {
       fill: '#fff',
@@ -162,15 +229,16 @@ var playState = {
       fontSize: 50
     })
     this.fishScoreText.setShadow(2, 2, 'rgba(6,68,142,0.7)', 1)
+    this.fishScoreText.x = -65
 
-    this.ddd = new Phaser.Rectangle(172, 35 ,109, 44)
+    this.ddd = new Phaser.Rectangle(172, 35, 109, 44)
   },
 
   update: function() {
-    // var d = this.fishes.getFirstDead()
-    // if (d) {
-    //   console.log(d)
-    // }
+    var d = this.fishes.getFirstDead()
+    if (d) {
+      this.fishes.remove(d, true, true)
+    }
   },
 
   render: function() {
@@ -178,18 +246,46 @@ var playState = {
   },
 
   _fishing: false,
-  _fishScore: 20,
-  _fishCount: 2,
+  _fishScore: 0,
+  _fishCount: 0,
   _currentCoin: 88888888,
   _jackpot: 12345678,
   _currentBet: 100,
   _currentWin: 0,
+
+  _updateFishCount: function() {
+    this._fishCount++
+    this.fishCountText.setText(this._fishCount)
+
+    this.fishCountGroup.x = -187
+    this.fishCountGroup.alpha = 0
+    game.add.tween(this.fishCountGroup).to(
+      {
+        x: 0,
+        alpha: 1
+      },
+      500,
+      Phaser.Easing.Cubic.Out,
+      true
+    )
+  },
+
+  _updateFishScore: function(sprite, tween, score) {
+    this._fishScore += score
+    this.fishScoreText.setText('+ ' + this._fishScore)
+  },
+
   _changeBet: function(v) {
     this._currentBet = v
     this.notchBetText.setText(v)
-    game.add.tween(this.betPop.position).to({
-      y: 270
-    }, 500, Phaser.Easing.Back.Out, true)
+    game.add.tween(this.betPop.position).to(
+      {
+        y: 270
+      },
+      500,
+      Phaser.Easing.Back.Out,
+      true
+    )
   },
 
   _throwBtnDown: function() {
@@ -198,24 +294,56 @@ var playState = {
       return
     }
     this.throwBtn.animations.frame = 1
-    
+
     this._throwPole()
   },
 
   _throwPole: function() {
     this._fishing = true
     this.throwBtn.alpha = 0.5
-    game.add.tween(this.fishPole).to({
-      angle: 0
-    }, 500, Phaser.Easing.Back.Out, true)
+    game.add.tween(this.fishPole).to(
+      {
+        angle: 0
+      },
+      500,
+      Phaser.Easing.Back.Out,
+      true
+    )
+
+    // game.time.events.add(Phaser.Timer.SECOND * 3, )
   },
 
   _withdrawPole: function() {
     this._fishing = false
     this.throwBtn.alpha = 1
-    game.add.tween(this.fishPole).to({
-      angle: 90
-    }, 500, Phaser.Easing.Back.Out, true)
+
+    this._fishScore = 0
+    this._fishCount = 0
+
+    game.add.tween(this.fishPole).to(
+      {
+        angle: 90
+      },
+      500,
+      Phaser.Easing.Back.Out,
+      true
+    )
+
+    game.add.tween(this.fishCountGroup).to(
+      {
+        x: -187,
+        alpha: 0
+      },
+      500,
+      Phaser.Easing.Cubic.Out,
+      true
+    )
+
+    this.fishes.forEach(function(item) {
+      if (item.catched) {
+        item.kill()
+      }
+    })
   },
 
   _throwBtnUp: function() {
@@ -223,6 +351,9 @@ var playState = {
   },
 
   _notchBetDown: function() {
+    if (this._fishing) {
+      return
+    }
     // toggle bet pop
     var y = this.betPop.y
     var nY = 0
@@ -230,18 +361,93 @@ var playState = {
     if (y === 0) {
       nY = 270
     }
-    game.add.tween(this.betPop.position).to({
-      y: nY
-    }, 500, Phaser.Easing.Back.Out, true)
+    game.add.tween(this.betPop.position).to(
+      {
+        y: nY
+      },
+      500,
+      Phaser.Easing.Back.Out,
+      true
+    )
   },
 
-  _updateFishes: function() {
+  _updateFishesMove: function() {
     this.fishes.forEach(function(item) {
-      if (item.alive && game.rnd.sign() === 1) {
+      if (item.alive && game.rnd.sign() === 1 && !item.catched) {
         var headOnLeft = isHeadOnLeft(item.key)
-        item.body.velocity.x = headOnLeft ? -1 * randomInt(0, 200) : randomInt(0, 200)
-        item.body.gravity.x = headOnLeft ? -1 * randomInt(0, 100) : randomInt(0, 100)
+        item.body.velocity.x = headOnLeft
+          ? -1 * randomInt(10, 200)
+          : randomInt(10, 200)
+        item.body.gravity.x = headOnLeft
+          ? -1 * randomInt(10, 100)
+          : randomInt(10, 100)
       }
     })
+  },
+
+  _updateFishesAdd: function() {
+    var r = randomInt(1, 12)
+    var headOnLeft = r % 2 !== 0
+    var key = 'f' + r
+    var x = getInitFishX(headOnLeft, key)
+    var y = getInitFishY()
+    var fishItem = this.fishes.create(x, y, key)
+    fishItem.body.velocity.x = getInitFishVelocity(headOnLeft)
+    fishItem.body.gravity.x = getInitFishGravity(headOnLeft)
+    fishItem.animations.add('swim')
+    fishItem.animations.play('swim', 10, true)
+    fishItem.checkWorldBounds = true
+    fishItem.outOfBoundsKill = true
+    fishItem.anchor.setTo(0.5, 0.5)
+  },
+
+  _updateFishesCatch: function() {
+    if (!this._fishing) {
+      return
+    }
+
+    var len = this.fishes.children.length
+    var index = randomInt(0, len - 1)
+
+    var fish = this.fishes.children[index]
+    var headOnLeft = isHeadOnLeft(fish.key)
+    var get = false
+
+    // console.log(fish.x)
+
+    if (headOnLeft) {
+      if (
+        fish.x > game.world.centerX + fish.width / 2 &&
+        fish.x < game.world.centerX + fish.width + 50
+      ) {
+        get = true
+      }
+    } else {
+      if (
+        fish.x > game.world.centerX - fish.width - 50 &&
+        fish.x < game.world.centerX - fish.width / 2
+      ) {
+        get = true
+      }
+    }
+
+    if (get) {
+      fish.catched = true
+      fish.body.velocity.x = 0
+      fish.body.gravity.x = 0
+      var tw = game.add.tween(fish).to(
+        {
+          angle: headOnLeft ? 90 : -90,
+          x: game.world.centerX + 10,
+          y: 480
+        },
+        500,
+        Phaser.Easing.Linear.None,
+        true
+      )
+      tw.onComplete.addOnce(this._updateFishCount, this)
+      // 计算金币
+      tw.onComplete.addOnce(this._updateFishScore, this, 1, 10)
+    }
   }
 }
